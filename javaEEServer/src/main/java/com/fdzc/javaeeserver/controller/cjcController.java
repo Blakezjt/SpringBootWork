5f8a1759f43c800b8da83e7b819d51bd45c020f1
cjc0318 <2317399420@qq.com>
2024-05-28 10:01:05 +0800

    cjc次二次上传

--- javaEEServer/src/main/java/com/fdzc/javaeeserver/controller/cjcController.java
+++ javaEEServer/src/main/java/com/fdzc/javaeeserver/controller/cjcController.java
@@ -1,9 +1,11 @@
 package com.fdzc.javaeeserver.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class cjcController {
+    @GetMapping("/dsad")
     public static void main(String[] args) {
 
     }
