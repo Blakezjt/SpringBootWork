52143a86eaf0afdb85edf045ad0e6043b892a6b1
cjc0318 <2317399420@qq.com>
2024-05-28 09:37:19 +0800

    cjc测试

---
+++ javaEEServer/src/main/java/com/fdzc/javaeeserver/controller/cjcController.java
@@ -0,0 +1,10 @@
+package com.fdzc.javaeeserver.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class cjcController {
+    public static void main(String[] args) {
+
+    }
+}
