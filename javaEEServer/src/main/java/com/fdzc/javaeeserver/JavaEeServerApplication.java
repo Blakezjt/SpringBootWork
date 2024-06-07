5bcc4110b869fdf4deb4bc4a1922fb9de1daa40a
xiejianfei <812844228@qq.com>
2024-06-07 11:50:33 +0800

    xjf

--- javaEEServer/src/main/java/com/fdzc/javaeeserver/JavaEeServerApplication.java
+++ javaEEServer/src/main/java/com/fdzc/javaeeserver/JavaEeServerApplication.java
@@ -1,8 +1,10 @@
 package com.fdzc.javaeeserver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.fdzc.javaeeserver.mapper")
 @SpringBootApplication
 public class JavaEeServerApplication {
 
--- javaEEServer/src/main/java/com/fdzc/javaeeserver/service/LibService.java
+++ javaEEServer/src/main/java/com/fdzc/javaeeserver/service/LibService.java
@@ -1,9 +1,11 @@
 package com.fdzc.javaeeserver.service;
 
 import com.fdzc.javaeeserver.entity.Lib;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface LibService {
 
 List<Lib> getAllList();
