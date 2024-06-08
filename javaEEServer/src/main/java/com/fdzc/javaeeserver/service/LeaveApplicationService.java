e4d129b34b29b4d469e128b38a61126f9a942c4c
cjc0318 <2317399420@qq.com>
2024-06-08 16:51:08 +0800

    学生请假事务完善

--- javaEEServer/src/main/java/com/fdzc/javaeeserver/service/LeaveApplicationService.java
+++ javaEEServer/src/main/java/com/fdzc/javaeeserver/service/LeaveApplicationService.java
@@ -1,8 +1,15 @@
 package com.fdzc.javaeeserver.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.fdzc.javaeeserver.entity.LeaveApplication;
 
 public interface LeaveApplicationService extends IService<LeaveApplication> {
     public boolean add(LeaveApplication leaveApplication);
+    public IPage<LeaveApplication> teacherlist(Page<LeaveApplication> page);
+
+    public IPage<LeaveApplication> studentlist(Integer studentId, Page<LeaveApplication> page);
+
+    public boolean deletebyId(Integer id);
 }
